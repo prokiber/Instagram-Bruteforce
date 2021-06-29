58967e168d5bea2982affd452ed4fcb6e7af7887
prokiber <79110760+prokiber@users.noreply.github.com>
2021-06-29 18:25:26 +0000

    Delete Instagram directory

--- Instagram/wordlist.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-webbrowser.open('https://wiki.skullsecurity.org/Passwords') # go to this site
-
-print "###################################"
-print "##opening site for password files##"
-print "##      Enjoy Your Hacking       ##"
-print "###################################" 
